213edbe85283df518b11c269087a130a192aea15
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-19 10:36:06 -0300

    Add Config Database e Inicializando DB MySQL

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const cors = require('cors');  // Importando o pacote cors
-const { sequelize } = require('./config/database');
+const cors = require('cors');
+const { sequelizeSQLite, sequelizeMySQL } = require('./config/database');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 // Configuração do CORS
 app.use(cors({
-    origin: 'http://localhost:3001', // Permitir apenas requisições de localhost:3001 (frontend)
-    methods: ['GET', 'POST'], // Métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'] // Cabeçalhos permitidos
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
@@ -17,7 +17,12 @@ app.use(express.json());
 // Usando as rotas de autenticação
 app.use('/api', authRoutes);
 
-// Inicialização do servidor e sincronização do banco de dados
-sequelize.sync().then(() => {
+// Inicialização do servidor e sincronização dos bancos de dados
+Promise.all([
+    sequelizeSQLite.sync(),
+    sequelizeMySQL.sync()
+]).then(() => {
     app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+}).catch(error => {
+    console.error('Erro ao sincronizar os bancos de dados:', error);
 });
