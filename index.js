e8fdc7d8c8210880b82ca3cec0a1007a16318a44
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-19 09:58:53 -0300

    Autorizando Requisicões do Front End e Add CORS

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-// index.js - Ponto de entrada do app
 const express = require('express');
+const cors = require('cors');  // Importando o pacote cors
 const { sequelize } = require('./config/database');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+// Configuração do CORS
+app.use(cors({
+    origin: 'http://localhost:3001', // Permitir apenas requisições de localhost:3001 (frontend)
+    methods: ['GET', 'POST'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'] // Cabeçalhos permitidos
+}));
+
 app.use(express.json());
 
+// Usando as rotas de autenticação
 app.use('/api', authRoutes);
 
 // Inicialização do servidor e sincronização do banco de dados
