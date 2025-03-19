92e6ce5d06cdcb9f8bcc5a7145ed11f9a6cf003a
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-19 10:41:36 -0300

    Substituindo Sqlite pelo MySQL

--- models/User.js
+++ models/User.js
@@ -1,10 +1,19 @@
-// models/User.js - Modelo de Usuário
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/database');
+const { sequelizeSQLite, sequelizeMySQL } = require('../config/database');
 
-const User = sequelize.define('User', {
+// Escolha qual conexão usar. 
+// Aqui vou demonstrar como configurar para MySQL, mas você pode escolher a conexão SQLite conforme necessário.
+
+// Usando o Sequelize com MySQL
+const User = sequelizeMySQL.define('User', {
     username: { type: DataTypes.STRING, unique: true, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: false }
 });
+
+// Caso você queira usar o SQLite, basta descomentar a linha abaixo:
+// const User = sequelizeSQLite.define('User', {
+//     username: { type: DataTypes.STRING, unique: true, allowNull: false },
+//     password: { type: DataTypes.STRING, allowNull: false }
+// });
 
 module.exports = User;
